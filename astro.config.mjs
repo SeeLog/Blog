7adfd98483c739fda901a4592e6d8727e4350344
SeeLog <seelog693@gmail.com>
2024-05-31 23:56:41 +0900

    update settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,23 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-import partytown from "@astrojs/partytown";
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.seelog.me',
-  integrations: [mdx(), sitemap(), tailwind(), react(), partytown({
-    config: {
-      forward: ['dataLayer.push'],
-    }
-  })]
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    react(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
+  build: {
+    format: 'preserve',
+  },
 });
